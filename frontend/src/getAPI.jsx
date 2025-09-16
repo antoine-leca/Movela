e15575cd3f232ab3f270044eaee47523b80a67bb
antoine-leca <antoine.leca@laplateforme.io>
2025-09-16 09:06:00 +0200

    modification du lien front vers l'api

--- frontend/src/getAPI.jsx
+++ frontend/src/getAPI.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://movela.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://movela.onrender.com/api/';
 
 // Configuration axios avec interceptors
 const api = axios.create({
